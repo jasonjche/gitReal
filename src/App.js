ca043aa9f79ba2a7f0b62cc4f511c4ca35f34032
Jason He <jasonhe.md@gmail.com>
2023-07-27 22:16:35 -0700

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
+import ImageComponent from './components/ImageComponent';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -9,34 +10,19 @@ function App() {
       .then(data => setImages(data));
   }, []);
 
-  const handleImageClick = (image) => {
+  const handleImageClick = useCallback((image) => {
     let temp = image['ss.jpg'];
     image['ss.jpg'] = image['pic.jpg'];
     image['pic.jpg'] = temp;
     setImages([...images]);
-  }
+  }, [images]);
 
   return (
     <div className="grid grid-cols-1 gap-4 bg-black text-white">
       <h1 className="text-4xl text-center pt-4 font-bold">GitReal.</h1>
-      {images.slice().reverse().map((image, index) => {
-        let date = new Date(image.timestamp * 1000);
-        let formattedDate = date.toLocaleString();
-        return (
-          <div key={index} className="relative rounded overflow-hidden shadow-lg m-2">
-            <img className="object-cover" src={image['ss.jpg']} alt="" />
-            <div className="absolute top-0 right-0 w-1/4">
-              <img className="rounded w-full h-full object-cover" src={image['pic.jpg']} alt="" onClick={() => handleImageClick(image)}/>
-            </div>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{image.commit_message}</div>
-              <p className="text-white text-base">
-                {formattedDate} by <strong>{image.author}</strong>
-              </p>
-            </div>
-          </div>
-        );
-      })}
+      {images.slice().reverse().map((image, index) => (
+        <ImageComponent key={index} image={image} handleImageClick={handleImageClick} />
+      ))}
     </div>
   );
 }
---
+++ src/components/ImageComponent.js
@@ -0,0 +1,21 @@
+function ImageComponent({ image, handleImageClick }) {
+  let date = new Date(image.timestamp * 1000);
+  let formattedDate = date.toLocaleString();
+
+  return (
+    <div className="relative rounded overflow-hidden shadow-lg m-2">
+      <img className="object-cover" src={image['ss.jpg']} alt="" />
+      <div className="absolute top-0 right-0 w-1/4">
+        <img className="rounded w-full h-full object-cover" src={image['pic.jpg']} alt="" onClick={() => handleImageClick(image)} />
+      </div>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{image.commit_message}</div>
+        <p className="text-white text-base">
+          {formattedDate} by <strong>{image.author}</strong>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ImageComponent;
