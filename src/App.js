b5f025ea3d5dd22c9631ce034c1a8d7d0e3a9af2
Jason He <jasonhe.md@gmail.com>
2023-08-07 20:47:34 -0700

    lol

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
   }, []);
 
   const handleImageClick = useCallback((image) => {
-    let temp = image['ss.jpg'];
+    let test = image['ss.jpg'];
     image['ss.jpg'] = image['pic.jpg'];
-    image['pic.jpg'] = temp;
+    image['pic.jpg'] = test;
     setImages([...images]);
   }, [images]);
 
